43a8eca558e10c7406130e14a5af2604b608b6d1
Rudys Acosta <ncrousset926@gmail.com>
2024-11-12 15:40:46 -0500

    Db transaction

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -91,7 +91,21 @@ func (store *Store) TransfeTx(ctx context.Context, arg TransferTxParams) (Transf
 			return err
 		}
 
-		// TODO: update account' balance
+		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.FromAccountID,
+			Amount: -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.ToAccountID,
+			Amount: arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
 
 		return nil
 	})
